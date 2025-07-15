a9ed1a4c916f9a4a8b3aea4cbf46131201174ce9
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-07-15 18:33:16 +0300

    Initial version of Reservation page implemented with API consumption.

---
+++ src/project/SRP.WebUI/Controllers/BookATableController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SRP.WebUI.Constants;
+using SRP.WebUI.Dtos.Booking;
+using SRP.WebUI.Hooks.Jsons;
+
+namespace SRP.WebUI.Controllers;
+
+public class BookATableController(JsonService jsonService) : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateBookingDto dto)
+    {
+        await jsonService.PostAsync(ApiRoutes.Booking.Add, dto);
+        return RedirectToAction("Index","Default");
+    }
+}
